2249e016aca1b50e03e33356c97b7cc92e628d75
Elijah Odjegba <elijahodjegba@gmail.com>
2022-11-02 16:49:26 +0100

    Fix codebase

--- student.rb
+++ student.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, *sups)
-    super(*sups)
+  def initialize(classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization, *sups)
-    super(*sups)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
