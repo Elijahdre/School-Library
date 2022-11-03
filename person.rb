e8c5cd6dcea218254c3f445aef577c4815660fbb
Elijah Odjegba <elijahodjegba@gmail.com>
2022-11-03 18:03:26 +0100

    Final corrections

--- person.rb
+++ person.rb
@@ -2,10 +2,12 @@ require 'securerandom'
 require_relative 'decorators/nameable'
 require_relative 'decorators/capitalize_decorator'
 require_relative 'decorators/trimmer_decorator'
+require_relative 'rental'
+
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -13,14 +15,19 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
-    of_age || @parent_permission
+    of_age? || @parent_permission
   end
 
   def correct_name
     @name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   private
